8f90711ef07c5c5d9b62625f54d57dfc2cfb9342
Ani Balasubramaniam <ani@anionline.me>
2020-02-05 23:34:57 -0800

    1. Ensure that usermod takes effect with su %USER 2. Make all scripts executable

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-# https://www.davidpashley.com/articles/writing-robust-shell-scripts
 # Fail on any errors
 set -e
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,23 +1,25 @@
 #! /bin/bash
 
-# https://www.davidpashley.com/articles/writing-robust-shell-scripts
 # Fail on any errors
 set -e
 
 # Install docker if not found
 command -v docker || {
-    echo -n "Installing docker..."
+    echo "Installing docker..."
     curl -sSL https://get.docker.com | sh
     echo "done."
-    
+
     # Add current user to docker group
-    echo -n "Performing post-installation steps..."
+    echo "Performing post-installation steps..."
     sudo usermod -aG docker $USER
     echo "done."
+
+    # Have this usermod take effect now
+    su $USER
 }
 
 # Build the image
-echo -n "Building docker image..."
+echo "Building docker image..."
 BASEDIR=$(dirname "$0")
 pushd $BASEDIR
 docker build -t iron-gauntlet ..
@@ -25,6 +27,6 @@ popd
 echo "done."
 
 # Run the container in the backgrond and have it restart always
-echo -n "Installing container as daemon..."
+echo "Installing container as daemon..."
 docker run --name iron-gauntlet-daemon -d --restart always --privileged --network host -it iron-gauntlet
 echo "done."
